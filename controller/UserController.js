cd9c29db4ead49a2b882ef130d39f61dc59b4233
krunal-29 <krunalmistry7545@gmail.com>
2024-03-13 12:55:24 +0530

    "up"

--- controller/UserController.js
+++ controller/UserController.js
@@ -1,7 +1,8 @@
 const Register=(req,res)=>{
-    res.send(req.body)
+
+    res.send("resgiter")
 }
 const Login=(req,res)=>{
     res.send("Login")
 }
-module.exports={Register,Login}
+module.exports={Register,Login} 
